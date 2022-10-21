76b21fae4d55a700ff79888ec827d508dde13ec7
IrinaK <skorpya@yandex.ru>
2022-10-21 21:28:39 +0300

    js

--- JavaScript/Seminar_JS/js.js
+++ JavaScript/Seminar_JS/js.js
@@ -1,6 +1,9 @@
 <script>
-    let name1 = "Алевтина";
+    const name1 = "Алевтина";
     alert(`Привет, ${name1}`);
+
+    let name2 = "Алевтина";
+    alert(`Привет, ${name2}`);
 
     let n = 1;
     let m = 2;
